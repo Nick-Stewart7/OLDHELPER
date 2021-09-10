45db5890560e9179ea60bbd1088b31952b83509d
Nicholas Stewart <nsstewart7@gmail.com>
2021-09-10 19:29:02 -0400

    here we go again

---
+++ GunScrape.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
+@dataclass
+class Gun(repr=True):
+    """Class for Gun BANGBANG"""
+    Icon: str
+    Name: str
+    Quote: str
+    Quality: str
+    Type: str
+    DPS: float
+    Mag: int
+    Ammo: int
+    Damage: int
+    FRate: float
+    Reload: float
+    ProjSpeed: int
+    Range: int
+    Force: int
+    Spread: int
+    Notes: str
+
+def scrapeWiki():
+    url = "https://enterthegungeon.fandom.com/wiki/Guns"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content)
+    gunTable = soup.find('table')
+    gunList = gunTable.find_all('tr')
+    for i in range(len(gunList)):
+        gunData = []
+        for node in gunList[i].find_all('td'):
+            print(node)
+            gunData.append(node)
+            #make everything a gun class
+            #how to store gun class for later access
+            #does it have to be a class? just throw this in a database
+            #make other python that uses the database so web app
+            #does not have to do this init?
+            #this scrape is an init
+            #program will need to parse this data
+        
+def main():
+    scrapeWiki()
+if __name__ == '__main__':
+    main()
