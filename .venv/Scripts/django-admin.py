4025f5a9defcd2668280d4b02d1e4c6b7fa49428
Nicholas Stewart <nsstewart7@gmail.com>
2021-09-12 20:02:22 -0400

    yes

--- .venv/Scripts/django-admin.py
+++ .venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\nsste\Projects\GungeonHelper\.venv\Scripts\python.exe
+#!C:\Users\nsste\GungeonHelper\.venv\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
