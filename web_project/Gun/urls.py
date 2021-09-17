e1ef5d7d2896d326e36c768599b768c8cdea06b1
Nick-Stewart7 <nsstewart7@gmail.com>
2021-09-16 23:06:44 -0400

    template in place

--- web_project/Gun/urls.py
+++ web_project/Gun/urls.py
@@ -3,4 +3,5 @@ from Gun import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("Gun/<name>", views.gunDisplay, name="gunDisplay"),
 ]
--- web_project/Gun/views.py
+++ web_project/Gun/views.py
@@ -1,6 +1,20 @@
+import re
+from django.shortcuts import render
+from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Prepare to die Dragun")
+
+def gunDisplay(request, name):
+    return render(
+        request,
+        'Gun/gunDisplay.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
 
 # Create your views here.
